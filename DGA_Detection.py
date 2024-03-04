bb590b39c6d683bb5099bd52f370ec8931d5aa54
FlaBBB <f12345ff67@gmail.com>
2024-03-04 13:40:55 +0700

    fix: failed load model

--- DGA_Detection.py
+++ DGA_Detection.py
@@ -2,37 +2,38 @@ import json
 
 import tldextract
 from keras.layers import LSTM, Activation, Dense, Dropout, Embedding
-from keras.models import load_model
+from keras.models import Sequential, load_model
 from keras.preprocessing.sequence import pad_sequences
 
-# Removed unused import statement
-# import tensorflow as tf
 
-# load model and config
-conf = json.load(open("./config.json"))
-valid_chars = conf["valid_chars"]
-max_len = conf["max_len"]
-max_feature = conf["max_feature"]
-custom_objects = {
-    "Embedding": Embedding(max_feature, 128, input_length=max_len),
-    "LSTM": LSTM(128),
-    "Dense": Dense(1),
-    "Dropout": Dropout(0.5),
-    "Activation": Activation("sigmoid"),
-}
-model = load_model("./model/dga_detection.v1.keras")
+class DGADetection:
+    # load model and config
+    def __init__(
+        self, config_file="./config.json", model_file="./model/dga_detection.v1.h5"
+    ):
+        conf = json.load(open(config_file))
+        self.valid_chars = conf["valid_chars"]
+        self.max_len = conf["max_len"]
+        self.max_feature = conf["max_feature"]
+        self.model = Sequential()
+        self.model.add(Embedding(self.max_feature, 128, input_length=self.max_len))
+        self.model.add(LSTM(128))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(1))
+        self.model.add(Activation("sigmoid"))
+        self.model.compile(
+            loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
+        )
 
+        self.model.load_weights(model_file)
 
-def predict(domains, threshold=0.5):
-    domain = [
-        [valid_chars[ch] for ch in tldextract.extract(domain).domain]
-        for domain in domains
-    ]
-    domain = pad_sequences(domain, maxlen=max_len)
+    def predict(self, domains, threshold=0.5):
+        domain = [
+            [self.valid_chars[ch] for ch in tldextract.extract(domain).domain]
+            for domain in domains
+        ]
+        domain = pad_sequences(domain, maxlen=self.max_len)
 
-    predicted = model.predict(domain)
+        predicted = self.model.predict(domain)
 
-    return (predicted > threshold).astype(int)
-
-
-print(predict(["google.com", "google.com", "google.com"]))
+        return [int(x[0]) for x in (predicted > threshold).astype(int)]
